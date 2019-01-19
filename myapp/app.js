58eebc4533af213e3f2961d20015c50903206905
David <0909023@r.nl>
2019-01-19 18:12:32 +0100

    websockets authenticated

--- myapp/app.js
+++ myapp/app.js
@@ -12,6 +12,13 @@ const app = express();
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 var cors = require('cors');
+const socket_io    = require( "socket.io" );
+var cookie = require('cookie');
+var sharedsession = require("express-socket.io-session");
+
+
+const systemData = require('./modules/systemData');
+const Tap = require('./modules/Tap');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -142,4 +149,123 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+
+// Socket.io
+var io           = socket_io();
+
+io.use(sharedsession(sessionMiddleware, {
+  autoSave:true
+}));
+
+app.io           = io;
+
+
+
+
+// io.set('authorization', function (handshakeData, callback) {
+//   console.log(handshakeData.rawHeaders)
+//   callback(null, true); // error first, 'authorized' boolean second 
+// });
+
+
+io.set('authorization', function (data, accept) {
+  // check if there's a cookie header
+
+  if (data.headers.cookie) {
+      // if there is, parse the cookie
+
+      try {
+        data.cookie = cookie.parse(data.headers.cookie);
+
+        data.sessionID = data.cookie['user_sid'];
+
+        accept(null, true);
+        
+      } catch (error) {
+
+        return accept('', false);
+
+      }
+
+
+  } else {
+     // if there isn't, turn down the connection with a message
+     // and leave the function.
+     return accept('', false);
+  }
+  // accept the incoming connection
+});
+
+
+io.on('connection', function (socket) {
+
+  if(socket.handshake.session.user){
+
+  Tap.isRunning().then(response =>{
+    socket.emit('tap_status',response)
+  })
+
+  socket.on('stopTap', function(){
+  
+    Tap.stopTap().then(response =>{
+      response.msg="user stopped the tap"
+      socket.emit('tap_status',response)
+    })
+  
+  })
+  
+  
+socket.on('testApiConnection', function(){
+
+  Tap.testConnection().then(connection_status =>{
+
+    socket.emit('connection_status',connection_status)
+
+  }).catch(err =>{
+    socket.emit('connection_status',{success:false,message:'Network error'})
+  })
+
+  Tap.isRunning().then(response =>{
+
+    socket.emit('tap_status',response)
+  })
+})
+
+
+  socket.on('startTap', function(){
+    Tap.testConnection().then(response =>{
+      if(response.success){
+        Tap.startTap().then(result =>{
+          
+          socket.emit('tap_status',result)
+        })
+      }else{
+        socket.emit('tap_status',{success:false, current_status:"Tap not running", msg:'could not connect to API'})
+      }
+  }).catch(err =>{
+    socket.emit('tap_status',{success:false, current_status:"Tap not running", msg:'could not connect to API'})
+  })
+})
+
+
+//get system data each second
+  setInterval(function () {
+    systemData.getSystemData().then(systemData =>{
+      for(let key in systemData){
+        socket.emit(key, systemData[key])
+      }
+  }).catch( err =>{
+    console.log(err)
+  })
+  
+}, 1000); 
+
+}
+
+});
+
+
+
 module.exports = app;
+
